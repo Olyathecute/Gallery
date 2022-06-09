88d12604b427afbed41986af096d6dc58895754b
Olyathecute <72819725+Olyathecute@users.noreply.github.com>
2022-06-10 01:27:15 +0400

    gh-pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,6 @@ import { Image } from 'react-bootstrap'
 import { MyPhoto, AboutText } from '../info'
 
 export default function About() {
-  console.log(process?.env, window?.env)
   return (
     <div className="flex-grow-1 m-3">
       <Image className="me-2 w-25 rounded border border-dark float-start" src={MyPhoto} />
