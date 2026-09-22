3954a2e46a23ffff63ce8edadfeac800d0428df7
Olga Sitnikova <72819725+Olyathecute@users.noreply.github.com>
2026-09-22 12:37:41 +0400

    Added Loading in About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPhotosFetch } from '../redux/appState'
+import SpinnerComponent from '../components/SpinnerComponent'
 import { AboutText } from '../info'
 import { Image } from 'react-bootstrap'
 import './About.css'
 
 export default function About() {
-  const { photos } = useSelector(({ photos }) => photos)
+  const { photos, isLoading } = useSelector(({ photos }) => photos)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -14,28 +15,34 @@ export default function About() {
   }, [dispatch])
 
   return (
-    <div className=" p-0 mx-0 flex-grow-1 align-items-center d-flex flex-column">
-      <div className="row my-2 mx-0 flex-grow-1 d-flex justify-content-center align-items-center">
-        <div className="col-10">
-          <div className="text-center textSize">{AboutText}</div>
-        </div>
-      </div>
-
-      <div className="my-2 image-row flex-grow-1 d-flex justify-content-center align-items-center">
-        <div className="image-track">
-          <div className="image-group">
-            {photos.map((photo, index) => (
-              <Image key={photo.id} src={photo.urls.small} className={`moving-image${index === photos.length - 1 ? '-last' : ''}`} />
-            ))}
+    <>
+      {isLoading ? (
+        <SpinnerComponent />
+      ) : (
+        <div className=" p-0 mx-0 flex-grow-1 align-items-center d-flex flex-column">
+          <div className="row my-2 mx-0 flex-grow-1 d-flex justify-content-center align-items-center">
+            <div className="col-10">
+              <div className="text-center textSize">{AboutText}</div>
+            </div>
           </div>
 
-          <div className="image-group" aria-hidden="true">
-            {photos.map(photo => (
-              <Image key={`copy-${photo.id}`} src={photo.urls.small} className="moving-image" />
-            ))}
+          <div className="my-2 image-row flex-grow-1 d-flex justify-content-center align-items-center">
+            <div className="image-track">
+              <div className="image-group">
+                {photos.map((photo, index) => (
+                  <Image key={photo.id} src={photo.urls.small} className={`moving-image${index === photos.length - 1 ? '-last' : ''}`} />
+                ))}
+              </div>
+
+              <div className="image-group" aria-hidden="true">
+                {photos.map(photo => (
+                  <Image key={`copy-${photo.id}`} src={photo.urls.small} className="moving-image" />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
